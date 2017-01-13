1c34bef683863c0dc5a0cbbd9b1cf332b3c9f74c
wdwit <wdwit@vitalhealthsoftware.com>
2017-01-13 16:09:14 +0100

    add translations

---
+++ app/locales/nl/translations.js
@@ -0,0 +1,23 @@
+export default {
+  'home': {
+    'header': {
+      'title': 'Welkom bij de *** Manager',
+      'sub': 'U kunt hier uw *** beheren.'
+    }
+  },
+  'articles': {
+    'header': {
+      'title': 'Items',
+      'sub': 'U kunt hieronder zoeken om items te beheren.'
+    },
+    'search': {
+      'placeholder': 'Type om te zoeken'
+    }
+  },
+  'export': {
+    'header': {
+      'title': 'Import / export',
+      'sub': 'U kunt hier een import en/of export doen.'
+    }
+  }
+};
